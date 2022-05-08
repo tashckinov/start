4ba790108d14148c45d7e06425eb32092457fb42
Max <ya.max1999-2010@yandex.ru>
2022-05-08 16:38:59 +0300

    feat(app.js): implemented adding/removing engines

--- js/app.js
+++ js/app.js
@@ -5,16 +5,24 @@ function isEmpty(value) {
 
 var app = new Vue({
     el: '#app', data: {
-        message: 'Noject', sites: [{
-            name: "youtube", url: "youtube.com", desc: "videohosting", bind: "yt"
-        }, {
+        sites: [{name: "youtube", url: "youtube.com", desc: "video hosting", bind: "yt"}, {
             name: "github", url: "github.com", desc: "hosting of IT projects", bind: "gh"
-        }], search_line: '', engines: {
+        }],
+        search_line: '',
+        engines: {
             ya: "https://yandex.ru/search/?text=",
             google: "https://www.google.com/search?ie=UTF-8&q=",
+            brave: "https://search.brave.com/search?q=",
             ddg: "https://duckduckgo.com/?q=",
             searx: "https://searx.tiekoetter.com/search?q="
-        }, engines_show: [], result: [], add: false, to_remove: false, remove_index: -1, engine: 'ya'
+        },
+        engines_show: [],
+        result: [],
+        add: false,
+        to_remove: false,
+        remove_index: -1,
+        engine: 'ya',
+        engine_request: []
     }, methods: {
         search() {
             if (this.search_line[0] !== ':' && this.search_line[0] !== '/') {
@@ -22,18 +30,39 @@ var app = new Vue({
             } else if (this.search_line[0] === ':') {
                 if (this.result.length === 1) document.location.href = 'https://' + this.result[0]['url'];
             } else {
-                if (this.engines_show.length === 1) this.change_engine(this.engines_show[0]);
+                if (this.engines_show.length === 1 && !this.to_remove) this.change_engine(this.engines_show[0]);
                 if (this.add) {
                     this.sites.push(this.add)
                     localStorage.setItem('sites', JSON.stringify(this.sites))
-                    this.search_line = ''
+                    this.search_line = '';
                     this.add = false;
+                    this.result = [];
+
                 }
                 if (this.to_remove && this.result.length === 1) {
                     this.sites.splice(this.remove_index, 1)
-                    this.result = []
-                    this.search_line = ''
                     localStorage.setItem('sites', JSON.stringify(this.sites))
+                    this.remove_index = -1;
+                    this.search_line = '';
+                    this.to_remove = false;
+                    this.result = [];
+                }
+                if (this.engine_request.length) {
+                    this.engines[this.engine_request[0]] = this.engine_request[1]
+                    localStorage.setItem('engines', JSON.stringify(this.engines))
+                    this.engines_show = [];
+                    this.engine_request = [];
+                    this.search_line = '';
+                }
+                if (this.engines_show.length === 1 && this.to_remove) {
+                    delete this.engines[this.engines_show[0]]
+                    localStorage.setItem('engines', JSON.stringify(this.engines))
+
+                    let keys = Object.keys(this.engines);
+                    if (localStorage.getItem('engine') === this.engines_show[0]) localStorage.setItem('engine', keys[keys.length * Math.random() << 0])
+
+                    this.engines_show = [];
+                    this.search_line = '';
                     this.to_remove = false;
                 }
             }
@@ -53,9 +82,9 @@ var app = new Vue({
         }, change_engine(engine) {
             if (engine in this.engines) {
                 localStorage.setItem('engine', engine)
-                this.search_line = '';
                 document.getElementById('line').focus()
                 this.engines_show = [];
+                this.search_line = '';
             }
         }, bind() {
             this.result = [];
@@ -69,12 +98,15 @@ var app = new Vue({
             let argument = this.search_line.slice(1).split(' ')[1]
             switch (command) {
                 case 'e':
+                case 'engine':
                     this.scmd_e(argument);
                     break;
                 case 'a':
+                case 'add':
                     this.scmd_a(argument);
                     break;
                 case 'r':
+                case 'remove':
                     this.scmd_r(argument);
                     break;
                 default:
@@ -90,6 +122,17 @@ var app = new Vue({
                 }
             }
         }, scmd_a(argument) {
+
+            if (argument === 'e' || argument === 'engine') {
+                let name = this.search_line.slice(1).split(' ')[2];
+                let url = this.search_line.slice(1).split(' ')[3];
+
+                if (!isEmpty(name) && !isEmpty(url)) {
+                    this.engine_request = [name, url]
+                }
+                return;
+            }
+
             let name = argument;
             let url = this.search_line.slice(1).split(' ')[2];
             let bind = this.search_line.slice(1).split(' ')[3];
@@ -98,17 +141,31 @@ var app = new Vue({
             if (!isEmpty(name) && !isEmpty(url) && !isEmpty(bind)) {
                 this.add = {name: name, url: url, desc: desc, bind: bind};
             } else this.add = false;
-        }, scmd_r(argument) {
-            this.result = [];
 
-            if (!isEmpty(argument)) {
-                for (let key in this.sites) {
-                    if (this.sites[key]['name'].includes(argument)) {
-                        this.result.push(this.sites[key]);
-                        this.remove_index = key;
-                        console.log(key);
-                        this.to_remove = true;
+        }, scmd_r(argument) {
+            this.engines_show = []
+            this.to_remove = false;
+            if (argument === 'e' || argument === 'engine') {
+                let name = this.search_line.slice(1).split(' ')[2];
+                if (!isEmpty(name)) {
+                    for (let key in this.engines) {
+                        if (key.includes(name)) {
+                            this.engines_show.push(key);
+                            this.to_remove = true;
+                        }
                     }
+                }
+                return;
+            }
+            this.result = [];
+            if (isEmpty(argument)) {
+                return;
+            }
+            for (let key in this.sites) {
+                if (this.sites[key]['name'].includes(argument)) {
+                    this.result.push(this.sites[key]);
+                    this.remove_index = key;
+                    this.to_remove = true;
                 }
             }
         }
@@ -120,6 +177,11 @@ var app = new Vue({
             localStorage.setItem('sites', JSON.stringify(this.sites))
         } else {
             this.sites = JSON.parse(localStorage.getItem('sites'));
+        }
+        if (!localStorage.getItem('engines')) {
+            localStorage.setItem('engines', JSON.stringify(this.engines))
+        } else {
+            this.engines = JSON.parse(localStorage.getItem('engines'));
         }
     }
 })
